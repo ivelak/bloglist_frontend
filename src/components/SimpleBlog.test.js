0f78a0bc54042822c08ce494cd14d3003db68c19
ivelak <rantaana@gmail.com>
2018-03-03 23:58:52 +0200

    5.13

--- src/components/SimpleBlog.test.js
+++ src/components/SimpleBlog.test.js
@@ -9,12 +9,34 @@ describe.only('<SimpleBlog />', () => {
             author: 'testikirjoittaja',
             likes: 5
         }
-        const blogComponent = shallow(<SimpleBlog blog={blog}/>)
+        const blogComponent = shallow(<SimpleBlog blog={blog} />)
         const titleDiv = blogComponent.find('.title')
         const likesDiv = blogComponent.find('.likes')
 
         expect(titleDiv.text()).toContain(blog.title)
         expect(titleDiv.text()).toContain(blog.author)
         expect(likesDiv.text()).toContain(blog.likes)
+    })
+    it('clicking the like button twice calls event handler twice', () => {
+        const blog = {
+            title: 'testiotsikko',
+            author: 'testikirjoittaja',
+            likes: 5
+        }
+
+        const mockHandler = jest.fn()
+
+        const blogComponent = shallow(
+            <SimpleBlog
+                blog={blog}
+                onClick={mockHandler}
+            />
+        )
+
+        const button = blogComponent.find('button')
+        button.simulate('click')
+        button.simulate('click')
+
+        expect(mockHandler.mock.calls.length).toBe(2)
     })
 })
