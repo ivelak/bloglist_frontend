f483abdc3a432f47f4a6e2930f1dbacb8316e008
ivelak <rantaana@gmail.com>
2018-03-03 23:17:37 +0200

    5.11

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,34 +1,38 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-const loginForm = ({handleSubmit, handleChange, username, password}) => (
+const loginForm = ({ handleSubmit, handleChange, username, password }) => (
     <div>
+        <h2>Kirjaudu</h2>
 
-      
-
-      <h2>Kirjaudu</h2>
-
-      <form onSubmit={handleSubmit}>
-        <div>
-          käyttäjätunnus
+        <form onSubmit={handleSubmit}>
+            <div>
+                käyttäjätunnus
           <input
-            type="text"
-            name="username"
-            value={username}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          salasana
+                    type="text"
+                    name="username"
+                    value={username}
+                    onChange={handleChange}
+                />
+            </div>
+            <div>
+                salasana
           <input
-            type="password"
-            name="password"
-            value={password}
-            onChange={handleChange}
-          />
-        </div>
-        <button>kirjaudu</button>
-      </form>
+                    type="password"
+                    name="password"
+                    value={password}
+                    onChange={handleChange}
+                />
+            </div>
+            <button>kirjaudu</button>
+        </form>
     </div>
-  )
+)
+loginForm.propTypes = {
+    handleSubmit: PropTypes.func.isRequired,
+    handleChange: PropTypes.func.isRequired,
+    username: PropTypes.string.isRequired,
+    password: PropTypes.string.isRequired
+}
 
-  export default loginForm
+export default loginForm
