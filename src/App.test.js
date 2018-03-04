b089f35635d91cb9e8c96426a3400e53c62ba265
ivelak <rantaana@gmail.com>
2018-03-04 21:33:19 +0200

    5.16

--- src/App.test.js
+++ src/App.test.js
@@ -5,18 +5,41 @@ import Blog from './components/Blog'
 import LoginForm from './components/LoginForm'
 jest.mock('./services/blogs')
 import blogService from './services/blogs'
+import login from './services/login';
 
-describe.only('<App />', () => {
+describe('<App />', () => {
     let app
-    beforeAll(() => {
-        app = mount(<App />)
+
+    describe('when user is not logged', () => {
+        beforeEach(() => {
+            app = mount(<App />)
+        })
+        it('only login form is rendered', () => {
+            app.update()
+            const blogComponents = app.find(Blog)
+            const loginComponents = app.find(LoginForm)
+            expect(blogComponents.length).toEqual(0)
+            expect(loginComponents.length).toEqual(1)
+        })
+    })
+    describe('when user is logged in', () => {
+        const user = {
+            username: 'tester',
+            token: '1231231214',
+            name: 'Teuvo Testaaja'
+        }
+        beforeEach(() => {
+            app = mount(<App />)
+            localStorage.setItem('loggedBlogappUser', JSON.stringify(user))
+
+        })
+        it('all blogs are rendered', () => {
+            app.update()
+            const blogComponents = app.find(Blog)
+
+            expect(blogComponents.length).toEqual(blogService.blogs.length)
+        })
     })
 
-    it('does not show any blogs before logging in', () => {
-        app.update()
-        const blogComponents = app.find(Blog)
-        const loginComponents = app.find(LoginForm)
-        expect(blogComponents.length).toEqual(0)
-        expect(loginComponents.length).toEqual(1)
-    })
+
 })
--- src/services/__mocks__/blogs.js
+++ src/services/__mocks__/blogs.js
@@ -2,42 +2,43 @@ let token = null
 
 const blogs = [
     {
-      id: "5a451df7571c224a31b5c8ce",
-      title: "HTML on helppoa",
-      author: "Kaikkonen",
-      likes: 2,
-      user: {
-        _id: "5a437a9e514ab7f168ddf138",
-        username: "mluukkai",
-        name: "Matti Luukkainen"
-      }
+        id: "5a451df7571c224a31b5c8ce",
+        title: "HTML on helppoa",
+        author: "Kaikkonen",
+        likes: 2,
+        user: {
+            _id: "5a437a9e514ab7f168ddf138",
+            username: "mluukkai",
+            name: "Matti Luukkainen"
+        }
     },
     {
-      id: "5a451e21e0b8b04a45638211",
-      title: "HTML on paskaa",
-      author: "Kekkonen",
-      likes: 1,
-      user: {
-        _id: "5a437a9e514ab7f168ddf138",
-        username: "mluukkai",
-        name: "Matti Luukkainen"
-      }
+        id: "5a451e21e0b8b04a45638211",
+        title: "HTML on paskaa",
+        author: "Kekkonen",
+        likes: 1,
+        user: {
+            _id: "5a437a9e514ab7f168ddf138",
+            username: "mluukkai",
+            name: "Matti Luukkainen"
+        }
     },
     {
-      id: "5a451e30b5ffd44a58fa79ab",
-      title: "mikä vitun HLTM",
-      author: "Leinonen",
-      likes: 1,
-      user: {
-        _id: "5a437a9e514ab7f168ddf138",
-        username: "mluukkai",
-        name: "Matti Luukkainen"
-      }
+        id: "5a451e30b5ffd44a58fa79ab",
+        title: "mikä vitun HLTM",
+        author: "Leinonen",
+        likes: 1,
+        user: {
+            _id: "5a437a9e514ab7f168ddf138",
+            username: "mluukkai",
+            name: "Matti Luukkainen"
+        }
     }
-  ]
+]
 
 const getAll = () => {
+    
     return Promise.resolve(blogs)
-  }
-  
-  export default { getAll, blogs }
+}
+
+export default { getAll, blogs }
